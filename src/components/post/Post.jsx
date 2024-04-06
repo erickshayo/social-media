4a06756d5c489928d16795b9d0f6ffa281781c3f
erickshayo <erickshayo306@gmail.com>
2024-04-06 23:22:14 +0300

    first commit

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,10 +1,19 @@
 import "./post.css"
+import {MoreVert} from "@mui/icons-material"
 
 export default function Post() {
   return (
     <div className="post">
         <div className="postWrapper">
-            <div className="postTop"></div>
+            <div className="postTop">
+                <div className="postTopLeft">
+                    <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
+                    <span className="postUsername">Sanel sanel</span>
+                    <span className="postDate">5 min Ago</span>
+                </div>
+                <div className="postTopRight"></div>
+                <MoreVert/>
+            </div>
             <div className="postCenter"></div>
             <div className="postBottom"></div>
         </div>
