5e415ab37104bdeaf2e297a2dc61ec0f28883454
erickshayo <erickshayo306@gmail.com>
2024-04-15 17:31:06 +0300

    commits

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import "./rightbar.css"
 import { Users } from "../../dummyData"
 import Online from "../online/Online"
 
-export default function Rightbar() {
+export default function Rightbar({Profile}) {
   return (
     <div className="rightbar">
       <div className="rightbarWrapper">
